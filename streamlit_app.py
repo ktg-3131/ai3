30ef0b021ae4d4f39a7ef4238690f3077799e856
ktg-3131 <H098S02804@chunchon.gwe.hs.kr>
2025-11-17 09:17:18 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -93,9 +93,9 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
 
 
 
-       labels[3]: {
+   labels[3]: {
        "texts": ["이승만은 부정선거 출신입니다."],
-       "image": ["https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pennmike.com%2Fnews%2FarticleView.html%3Fidxno%3D54371&psig=AOvVaw3cgrJdeLBd9xjP5AxGK7bO&ust=1763424655067000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCLiFh_jy95ADFQAAAAAdAAAAABAE"] 
+       "images": ["https://www.google.com/imgres?q=%EC%9D%B4%EC%8A%B9%EB%A7%8C&imgurl=https%3A%2F%2Fcdn.pennmike.com%2Fnews%2Fphoto%2F202205%2F54371_74041_2841.jpg&imgrefurl=https%3A%2F%2Fwww.pennmike.com%2Fnews%2FarticleView.html%3Fidxno%3D54371&docid=QQKcLPrQMSMX3M&tbnid=yxCBAdvAqbo5aM&vet=12ahUKEwjZh6eT9PeQAxVcZvUHHb_pKT0QM3oECBcQAA..i&w=600&h=850&hcb=2&ved=2ahUKEwjZh6eT9PeQAxVcZvUHHb_pKT0QM3oECBcQAA"]
        "videos": ["https://www.youtube.com/watch?v=HcN4-pZeBnM"]
      },
 
