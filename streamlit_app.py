4129b0bdafd4d8541e11764bf132bb54ea7328b9
ktg-3131 <H098S02804@chunchon.gwe.hs.kr>
2025-11-18 13:47:44 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,7 +71,7 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
     
     labels[0]: {
        "texts": ["박정희는 총을 맞고 죽었습니다"],
-       "images": ["https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.joongang.co.kr%2Farticle%2F25220202&psig=AOvVaw1jr3Ym3jpQE4iE8gk0ywBn&ust=1763423776506000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCLjx89Hv95ADFQAAAAAdAAAAABAE"],
+       "images": ["https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Park_Chung_Hee_%28%EB%B0%95%EC%A0%95%ED%9D%AC%29_Presidential_Portrait.jpg/250px-Park_Chung_Hee_%28%EB%B0%95%EC%A0%95%ED%9D%AC%29_Presidential_Portrait.jpg"],
        "videos": ["https://www.youtube.com/watch?v=7sWXH4IbHKs"]
      },
 
